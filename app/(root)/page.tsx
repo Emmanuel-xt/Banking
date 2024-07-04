b807ef7491f2a92fd8ab1c9086fe0049273ae5d4
Emmanuel-xt <fattmane5@gmail.com>
2024-07-04 18:30:35 +0100

    Implement chatjs @ home

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import Header from "@/components/Header";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+import React from "react";
 
 const Home = () => {
+  const loggedIn = {
+    firstname: "Emmanuel",
+  };
   return (
-    <div>Home</div>
-  )
-}
+    <section className="home">
+      <div className="home-content">
+        <Header
+          type="greeting"
+          title="Welcome"
+          user={loggedIn?.firstname || "Guest"}
+          subtext="Access and manage your accounts seamlessly"
+        />
+        <TotalBalanceBox
+          accounts={[]}
+          totalBanks={1}
+          totalCurrentBalance={1250.35}
+        />
+      </div>
+    </section>
+  );
+};
 
-export default Home
+export default Home;
